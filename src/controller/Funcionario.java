715dc87ed7120ff66e0514ef371e6ec8e6e5a694
Mateus Couto <mateuscouto98@outlook.com>
2021-11-14 07:04:34 -0300

    Finalizando as classes

---
+++ src/controller/Funcionario.java
@@ -0,0 +1,72 @@
+
+package controller;
+
+/**
+ *
+ * @author Mateus Couto
+ */
+public class Funcionario {
+    private int id_funcionario;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String dataAdmissao;
+    private String senha;
+
+    public int getId_funcionario() {
+        return id_funcionario;
+    }
+
+    public void setId_funcionario(int id_funcionario) {
+        this.id_funcionario = id_funcionario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDataAdmissao() {
+        return dataAdmissao;
+    }
+
+    public void setDataAdmissao(String dataAdmissao) {
+        this.dataAdmissao = dataAdmissao;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
--- src/controller/Medico.java
+++ src/controller/Medico.java
@@ -5,5 +5,10 @@ package controller;
  * @author Mateus Couto
  */
 public class Medico {
-    
+    private int id_paciente;
+    private String nome;
+    private String crm;
+    private String email;
+    private String telefone;
+    private String especializacao;
 }
